501098a19b542937536246736de6ea6fc8b7f0f4
eldesh <nephits@gmail.com>
2025-12-02 20:21:27 +0900

    Misc: AgdaProcess

--- agda.py
+++ agda.py
@@ -132,14 +132,7 @@ class AgdaProcess:
     def restart(self, path):
         '''Terminates the current Agda process and starts a new one located at `path`.'''
         self.stopWait()
-        self._path = path
-        self._process = subprocess.Popen(
-            [self._path, "--interaction"],
-            bufsize = 1,
-            stdin = subprocess.PIPE,
-            stdout = subprocess.PIPE,
-            universal_newlines = True
-        )
+        self = AgdaProcess(path)
 
     def stopWait(self):
         '''Terminates the current Agda process and waits for it to exit. If it does not exit within 10 seconds, it is killed.'''
@@ -147,10 +140,12 @@ class AgdaProcess:
         try:
             self._process.wait(timeout = 10)
         except subprocess.TimeoutExpired:
+            logger.error("Agda process did not exit in time, killing it.")
             self._process.kill()
             self._process.wait()
         self._process = None
         self._path = None
+        self._version = None
 
 
 python_cmd = 'py' if version_info.major == 2 else 'py3'
