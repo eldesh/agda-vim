5a847984a20af1a12bff0509e1af7602a91e2687
eldesh <nephits@gmail.com>
2025-12-28 22:57:43 +0900

    Add: type annotaions
    - Add: type annotations
    - Fix: unknown escape sequence '\&'.

--- pythonx/agdavim/agda_path.py
+++ pythonx/agdavim/agda_path.py
@@ -1,22 +1,22 @@
 from typing import Iterator
 
-def agda2_quote_char(c):
+def agda2_quote_char(c : str) -> str:
     '''Convert a character to its Haskell escaped representation.'''
     ord_c = ord(c)
     if ord_c < 128:
         return c
-    return "\\x%x\&" % ord_c
+    return "\\x%x&" % ord_c
 
 def agda2_quote_list(ss: Iterator[str]) -> str:
     '''Convert a list of strings to its Haskell string list representation.'''
     return '[' + ', '.join(escape(s) for s in ss) + ']'
 
 # This technically needs to turn a string into a Haskell escaped string, buuuut just gonna cheat.
-def escape(s):
+def escape(s: str):
     estr = s.replace('\\', '\\\\').replace('"', '\\"').replace('\n','\\n')
     return '"' + ''.join(agda2_quote_char(c) for c in estr) + '"'
 
 # This technically needs to turn a Haskell escaped string into a string, buuuut just gonna cheat.
-def unescape(s):
+def unescape(s: str):
     return s.replace('\\\\','\x00').replace('\\"', '"').replace('\\n','\n').replace('\x00', '\\') # hacktastic
 
